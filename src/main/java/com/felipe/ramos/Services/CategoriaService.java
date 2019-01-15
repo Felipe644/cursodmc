aae350b1387ec02bf45c5463d377bd785f043e31
Felipe <felipeguilherme644@gmail.com>
2019-01-15 14:03:27 -0200

    Essa Vai

--- src/main/java/com/felipe/ramos/Services/CategoriaService.java
+++ src/main/java/com/felipe/ramos/Services/CategoriaService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.felipe.ramos.Repository.CategoriaRepository;
+import com.felipe.ramos.Services.exception.ObjectNotFoundException;
 import com.felipe.ramos.domain.Categoria;
 
 @Service
@@ -16,7 +17,11 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj= repo.findById(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! id: "+id+", Tipo: "+Categoria.class.getName());
+		}
 		return obj.orElse(null);
+
 		
 	}
 
